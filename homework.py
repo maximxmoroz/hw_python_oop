248badbb8fa2429f43b89991dbec198d20982eb1
maximxmoroz <maximxmoroz@gmail.com>
2021-10-05 20:38:05 +0500

    project

--- homework.py
+++ homework.py
@@ -1,9 +1,71 @@
 import datetime as dt
-
-
-class Calculator:
-    pass
-
-
+FORMAT = '%d.%m.%Y'
 class Record:
-    pass
+    def __init__(self, amount, comment, date = None):
+        self.amount = amount 
+        self.comment = comment
+        if date is None:
+            self.date = dt.datetime.now().date()
+        else:    
+            self.date = dt.datetime.strptime(date, FORMAT).date()
+class Calculator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+    def add_record(self, Record): 
+        self.records.append(Record)
+    def get_today_stats(self):
+        now = dt.datetime.now().date()
+        result = sum([record.amount for record in self.records
+                     if record.date == now])
+        return result             
+    def get_week_stats(self):
+        '''days=7'''
+        now = dt.datetime.now().date()
+        past_week = now - dt.timedelta(7)
+        week_result = sum(record.amount for record in self.records
+                          if past_week <= record.date <= now)
+        return week_result                  
+class CashCalculator(Calculator):
+     RUB_RATE = 1.0
+     USD_RATE = 60.0
+     EURO_RATE = 70.0
+                  
+     def get_today_cash_remained(self, hello=None):
+          cash_result = self.get_today_stats()
+          currency_dict = {'rub': (1.0, 'руб'),
+                           'usd': (self.USD_RATE, 'USD'),
+                           'eur': (self.EURO_RATE, 'Euro'),
+          }
+          abbr, coin = currency_dict[hello] 
+          fiat = round((self.limit-cash_result)/abbr,2)
+          if cash_result < self.limit:
+               output = f'На сегодня осталось {fiat} {coin}'
+          elif cash_result == self.limit: 
+               output = 'Денег нет, держись'
+          else:
+               if fiat < 0:
+                    cash = abs(fiat)
+                    output = f'Денег нет, держись: твой долг - {cash} {coin}'     
+          return output            
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        calories_result = self.limit - self.get_today_stats()
+        if calories_result > 0:
+            return ('Сегодня можно съесть что-нибудь ещё,'
+                    f' но с общей калорийностью не более {calories_result} кКал')
+        return 'Хватит есть!'
+
+
+cash_calculator = CashCalculator(1000)
+# дата в параметрах не указана,
+# так что по умолчанию к записи
+# должна автоматически добавиться сегодняшняя дата
+cash_calculator.add_record(Record(amount=145, comment='кофе'))
+# и к этой записи тоже дата должна добавиться автоматически
+cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+# а тут пользователь указал дату, сохраняем её
+cash_calculator.add_record(Record(amount=3000,
+                                  comment='бар в Танин др',
+                                  date='08.11.2019'))
+print(cash_calculator.get_today_cash_remained('rub'))
