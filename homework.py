b130c737f013988ee0711ae716a8401641818271
maximxmoroz <maximxmoroz@gmail.com>
2021-10-05 20:52:54 +0500

    project3

--- homework.py
+++ homework.py
@@ -1,6 +1,7 @@
 import datetime as dt
 FORMAT = '%d.%m.%Y'
-class Record:
+
+class Record: 
     def __init__(self, amount, comment, date = None):
         self.amount = amount 
         self.comment = comment
@@ -8,17 +9,21 @@ class Record:
             self.date = dt.datetime.now().date()
         else:    
             self.date = dt.datetime.strptime(date, FORMAT).date()
+
 class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
+
     def add_record(self, Record): 
         self.records.append(Record)
+
     def get_today_stats(self):
         now = dt.datetime.now().date()
         result = sum([record.amount for record in self.records
                      if record.date == now])
         return result             
+
     def get_week_stats(self):
         '''days=7'''
         now = dt.datetime.now().date()
@@ -26,6 +31,7 @@ class Calculator:
         week_result = sum(record.amount for record in self.records
                           if past_week <= record.date <= now)
         return week_result                  
+
 class CashCalculator(Calculator):
      RUB_RATE = 1.0
      USD_RATE = 60.0
@@ -48,7 +54,9 @@ class CashCalculator(Calculator):
                     cash = abs(fiat)
                     output = f'Денег нет, держись: твой долг - {cash} {coin}'     
           return output            
+
 class CaloriesCalculator(Calculator):
+
     def get_calories_remained(self):
         calories_result = self.limit - self.get_today_stats()
         if calories_result > 0:
