e90ff7e619be28234d05784663cb3f4fbf2d87aa
maximxmoroz <maximxmoroz@gmail.com>
2021-10-05 21:19:39 +0500

    project8

--- homework.py
+++ homework.py
@@ -38,30 +38,30 @@ class Calculator:
 class CashCalculator(Calculator):
     RUB_RATE = 1.0
     USD_RATE = 60.0
-    EURO_RATE = 70.0
-                  
+    EURO_RATE = 70.0             
     def get_today_cash_remained(self, hello=None):
         cash_result = self.get_today_stats()
         currency_dict = {'rub': (1.0, 'руб'),
                          'usd': (self.USD_RATE, 'USD'),
                          'eur': (self.EURO_RATE, 'Euro')}
         abbr, coin = currency_dict[hello]
-        fiat = round((self.limit-cash_result)/abbr,2)
+        fiat = round((self.limit - cash_result) / abbr, 2)
         if cash_result < self.limit:
             output = f'На сегодня осталось {fiat} {coin}'
-        elif cash_result == self.limit: 
+        elif cash_result == self.limit:
             output = 'Денег нет, держись'
         else:
             if fiat < 0:
                 cash = abs(fiat)
-                output = f'Денег нет, держись: твой долг - {cash} {coin}'     
-        return output            
+                output = f'Денег нет, держись: твой долг - {cash} {coin}'
+        return output
+
 
 class CaloriesCalculator(Calculator):
 
     def get_calories_remained(self):
         calories_result = self.limit - self.get_today_stats()
         if calories_result > 0:
-            return ('Сегодня можно съесть что-нибудь ещё,'
-                    f' но с общей калорийностью не более {calories_result} кКал')
+            return ('Сегодня можно съесть что-нибудь'
+                    f' еще, но с общей калорийностью не более {calories_result} кКал')
         return 'Хватит есть!'
