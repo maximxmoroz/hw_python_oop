a8b4ea6a708922916ffa5c9981036b16f224fdf7
maximxmoroz <maximxmoroz@gmail.com>
2021-10-05 21:05:14 +0500

    project5

--- homework.py
+++ homework.py
@@ -1,13 +1,13 @@
 import datetime as dt
 FORMAT = '%d.%m.%Y'
 
-class Record: 
+class Record:
     def __init__(self, amount, comment, date=None):
-        self.amount = amount 
+        self.amount = amount
         self.comment = comment
         if date is None:
             self.date = dt.datetime.now().date()
-        else:    
+        else:
             self.date = dt.datetime.strptime(date, FORMAT).date()
 
 class Calculator:
@@ -15,14 +15,14 @@ class Calculator:
         self.limit = limit
         self.records = []
 
-    def add_record(self, Record): 
+    def add_record(self, Record):
         self.records.append(Record)
 
     def get_today_stats(self):
         now = dt.datetime.now().date()
         result = sum([record.amount for record in self.records
                      if record.date == now])
-        return result             
+        return result
 
     def get_week_stats(self):
         '''days=7'''
@@ -30,7 +30,7 @@ class Calculator:
         past_week = now - dt.timedelta(7)
         week_result = sum(record.amount for record in self.records
                           if past_week <= record.date <= now)
-        return week_result                  
+        return week_result   
 
 class CashCalculator(Calculator):
      RUB_RATE = 1.0
@@ -43,7 +43,7 @@ class CashCalculator(Calculator):
                            'usd': (self.USD_RATE, 'USD'),
                            'eur': (self.EURO_RATE, 'Euro'),
           }
-          abbr, coin = currency_dict[hello] 
+          abbr, coin = currency_dict[hello]
           fiat = round((self.limit-cash_result)/abbr,2)
           if cash_result < self.limit:
                output = f'На сегодня осталось {fiat} {coin}'
